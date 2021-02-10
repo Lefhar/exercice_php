26c888bfc376edd3682ce4ded5fa0380708e973c
harold <s.lefebvre907@laposte.net>
2021-02-10 15:43:28 +0100

    exercice php

---
+++ ListeLiens/index.php
@@ -0,0 +1,12 @@
+<?php
+$fp = fopen("ListeLiens.txt", "r");
+echo '<ul>';
+while (!feof($fp))
+{
+    // Lecture d'une ligne, le contenu de la ligne est affecté à la variable $ligne
+    $ligne = fgets($fp, 4096);
+    if(!empty($ligne)){
+    echo "\n<li><a href='".urldecode(trim($ligne))."' target='_blank'>".urldecode(trim($ligne))."</a> </li>\n";
+    }
+}
+echo '</ul>';
