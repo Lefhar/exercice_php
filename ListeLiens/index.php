ea66bbcb3584cb74325ce2b6d601dd0e2bd34b35
harold <s.lefebvre907@laposte.net>
2021-02-10 15:48:07 +0100

    exercice php

--- ListeLiens/index.php
+++ ListeLiens/index.php
@@ -6,7 +6,7 @@ while (!feof($fp))
     // Lecture d'une ligne, le contenu de la ligne est affecté à la variable $ligne
     $ligne = fgets($fp, 4096);
     if(!empty($ligne)){
-    echo "\n<li><a href='".urldecode(trim($ligne))."' target='_blank'>".urldecode(trim($ligne))."</a> </li>\n";
+    echo "\n<li><a href='".urldecode(trim($ligne))."' target='_blank'>".urldecode(trim($ligne))."</a></li>\n";
     }
 }
 fclose($fp);
